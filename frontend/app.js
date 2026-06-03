8dbac80b02cd7218dba7a62508fecba636cb13c9
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-03 15:05:23 -0500

    Se desarrollan los métodos de POST y PUT

--- frontend/app.js
+++ frontend/app.js
@@ -34,3 +34,59 @@ async function getPeople() {
         console.error('Error al obtener datos:', error);
     }
 }
+
+// 2. CREAR (POST) / ACTUALIZAR (PUT) - Manejo del Formulario
+personForm.addEventListener('submit', async (e) => {
+    e.preventDefault(); // Esto sirve para evitar que la página se recargue
+    
+    const id = personIdInput.value;
+    const name = personNameInput.value;
+    
+    const personData = { name: name };
+
+    if (id) {
+        // Si existe un Id, se usa el método de actualización (PUT)
+        try {
+            await fetch(`${API_URL}/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify(personData),
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
+            });
+            resetForm();
+            getPeople();
+
+        } catch (error) {
+            console.error('Error al actualizar:', error);
+        }
+    } else {
+        // Si no hay un Id, se usa el método de creación (POST)
+        try {
+             // 1. Obtenemos la lista actual para ver las personas existentes
+            const res = await fetch(API_URL);
+            const currentPeople = await res.json();
+            
+            // 2. Buscamos el Id con el valor más alto (si no hay ninguno, empezamos con el 1)
+            const maxId = currentPeople.reduce((max, p) => {
+                const currentId = parseInt(p.id);
+                return !isNaN(currentId) && currentId > max ? currentId : max;
+            }, 0);
+            
+            // 3. Le asignamos el siguiente número al Id del nuevo registro
+            const newPersonData = { 
+                id: maxId + 1, 
+                name: name 
+            };
+
+            await fetch(API_URL, {
+                method: 'POST',
+                body: JSON.stringify(newPersonData),
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
+            });
+
+            resetForm();
+            getPeople();
+        } catch (error) {
+            console.error('Error al guardar:', error);
+        }
+    }
+});
