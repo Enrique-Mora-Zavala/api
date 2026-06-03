8027a47af2a7a10e8482df2cd83c86147594488c
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-03 14:51:06 -0500

    Se llama la API y los elementos que conforman la página web

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+const API_URL = 'http://localhost:3000/people';
+
+// Elementos del DOM
+const personForm = document.getElementById('personForm');
+const personIdInput = document.getElementById('personId');
+const personNameInput = document.getElementById('personName');
+const submitBtn = document.getElementById('submitBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const peopleTableBody = document.getElementById('peopleTableBody');
